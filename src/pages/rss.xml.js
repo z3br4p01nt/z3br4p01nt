994220d4e582b2d4044695b2b085a6a4469b2281
z3br4p01nt <125839714+z3br4p01nt@users.noreply.github.com>
2023-04-20 17:20:23 +0200

    2 files modified, 1 added : Add an RSS feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function get() {
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'My journey learning Astro',
+    site: 'https://my-blog-site.netlify.app',
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
+}
